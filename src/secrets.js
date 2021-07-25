d6cd2fb9c01800cd236f48cff997b7677cb22801
avnish <avnishsingh9898@gmail.com>
2021-07-25 23:56:51 +0530

    commit

---
+++ src/secrets.js
@@ -0,0 +1,8 @@
+module.exports={
+    apiKey: "AIzaSyCN63T32KWUJUTMmollcnLmeyYoqcs9QxY",
+    authDomain: "reels2-fc302.firebaseapp.com",
+    projectId: "reels2-fc302",
+    storageBucket: "reels2-fc302.appspot.com",
+    messagingSenderId: "56209581196",
+    appId: "1:56209581196:web:a6c2afb2741f73704c87a9"
+}
